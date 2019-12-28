3a5c3183781cf0e06865749b55503188a17332f6
Pratima Gautam <astute.yard@gmail.com>
2019-12-28 08:50:21 +0545

    Bind scheduler data using remote service URL

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 # Scheduler-Component With React TypeScript
-- created an application with the help of the create react app tool. 
-- learned how to add appointments to the React Scheduler either as local JSON data or remote service URLs as well as how to change the default date and view in React Scheduler
+- Created an application with the help of the create react app tool. 
+- Learned how to add appointments to the React Scheduler either as local JSON data or remote service URLs as well as how to change the default date and view in React Scheduler
 - View Types:
     1. Day
     2. Week 
@@ -14,3 +14,6 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
     8. TImeline Week
     9. Timeline Work Week
     10. Timeline Month
+- Add REACT Scheduler
+- Change scheduler default date and view
+- Bind scheduler data using local JSON or remote service URL
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel} from '@syncfusion/ej2-react-schedule';
 import './App.css';
+import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel} from '@syncfusion/ej2-react-schedule';
+import {DataManager, WebApiAdaptor} from '@syncfusion/ej2-data';
+
 class App extends React.Component{
   private localData: EventSettingsModel ={
     dataSource: [{
@@ -8,9 +10,16 @@ class App extends React.Component{
       StartTime: new Date(2019, 11, 25, 4, 0) //YYYY, MM, DD, TIME: HOUR/MINUTES
     }]
   }
+
+  private remoteData = new DataManager({
+    url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+    adaptor: new WebApiAdaptor,
+    crossDomain: true
+  })
+
   public render(){
     return (
-      <ScheduleComponent currentView='Month' eventSettings={this.localData}>
+      <ScheduleComponent currentView='Month' selectedDate={new Date(2017, 5, 5)} eventSettings={{dataSource: this.remoteData}}>
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
       </ScheduleComponent>
     );
