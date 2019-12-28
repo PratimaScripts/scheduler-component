9b4df037dafeeda8926fe8ac22e7ed9225681407
Pratima Gautam <astute.yard@gmail.com>
2019-12-28 08:28:45 +0545

    Add appointment to empty scheduler

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda} from '@syncfusion/ej2-react-schedule';
+import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel} from '@syncfusion/ej2-react-schedule';
 import './App.css';
+class App extends React.Component{
+  private localData: EventSettingsModel ={
+    dataSource: [{
+      EndTime: new Date(2019, 11, 25, 6, 30),
+      StartTime: new Date(2019, 11, 25, 4, 0) //YYYY, MM, DD, TIME: HOUR/MINUTES
+    }]
+  }
+  public render(){
+    return (
+      <ScheduleComponent currentView='Month' eventSettings={this.localData}>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+      </ScheduleComponent>
+    );
+  }
 
-const App: React.FC = () => {
-  return (
-    <ScheduleComponent currentView='Month'>
-      <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
-    </ScheduleComponent>
-  );
 }
 
 export default App;
